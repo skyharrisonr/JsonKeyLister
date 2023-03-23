742ff82333b8a08351a1f1570bfac11c3124f61e
skyharrisonr <robson.harrison@sky.uk>
2023-03-23 09:41:11 +0000

    I've amended the Python script to allow multi level Json and also added a total count under the list of keys.

--- jsonKeys.py
+++ jsonKeys.py
@@ -7,7 +7,7 @@ Date: March 2023
 import json
 
 # Specify the path of the JSON file to read
-filename = 'example.json'
+filename = 'filepath'
 
 # Open the file and load the JSON data
 with open(filename) as f:
@@ -16,11 +16,22 @@ with open(filename) as f:
 # Create a set to store the unique keys
 keys = set()
 
-# Loop through each dictionary in the JSON data and add its keys to the set
-for d in data:
+# Define a function to recursively add keys to the set
+def add_keys(d):
     if isinstance(d, dict):
         keys.update(d.keys())
+        for v in d.values():
+            add_keys(v)
+
+# Loop through each item in the JSON data and add its keys to the set
+if isinstance(data, dict):
+    add_keys(data)
+elif isinstance(data, list):
+    for d in data:
+        add_keys(d)
 
 # Convert the set to a sorted list and print it
+unique_keys = sorted(list(keys))
 print("List of unique keys in the JSON file:")
 print(sorted(list(keys)))
+print(f"Total number of unique keys: {len(unique_keys)}")
